50c4b94fc2b8bae15cfc37cdd0847e2ce2f98d7d
weaverd <david.weaver@dpird.wa.gov.au>
2022-11-05 21:13:17 +0800

    Basic Bar Chart

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-//Here we're importing items we'll need. You can add other imports here.
+import Plotly from "plotly.js/dist/plotly";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+const Yvalue = [20, 14, 23, '','' ];
+const Xvalue = ['Very Low', 'Low', 'High', 'Other', 'Other2'];
+//const y=Yvalue;
+//const x=Xvalue;
+const trace1 = {
+  x: Xvalue,
+  y: Yvalue,
+  marker:{
+    color: ['red', 'orange', 'green', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)']
+  },
+  type: 'bar',
+  width: 0.9,
+ 
+  text: Yvalue.map(String),
+  textposition: "outside",
+  showticklabels: true
 };
 
-//
+const data = [trace1];
+
+const layout = {
+  title: 'Phosphorus Status',
+  width: 800,
+  height: 600,
+  font: {size: 14},
+  xaxis: {linewidth: 2, showgrid: 'major'},
+  yaxis: {linewidth: 2}
+};
+
+Plotly.newPlot('chart', data, layout, {displayModeBar: false});
